609f6dc4e9ff58cad0932aec1503b82426635386
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-05-12 19:49:44 -0300

    order submission and passing between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,34 +88,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = async () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Tiago Emerick',
-        //         address: {
-        //             street: 'NY5569',
-        //             zipCode: '1546546',
-        //             country: 'Brasil'
-        //         },
-        //         email: 'tiago@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         console.log(response);
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         console.log(error);
-        //         this.setState({loading: false, purchasing: false});
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price='+this.state.totalPrice);
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,22 +6,23 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients: null,
+        totalPrice: 0
     }
 
-    componentDidMount () {
+    componentWillMount () {
         const query = new URLSearchParams(this.props.location.search);
         const ingredient = {};
+        let price = 0;
         for (let param of query.entries()) {
             // [salad, 1]
-            ingredient[param[0]] = +param[1];
+            if (param[0] === 'price') {
+                price = param[1];
+            } else {
+                ingredient[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredient});
+        this.setState({ingredients: ingredient, totalPrice: price});
     }
 
     checkoutCancelledHandler = () => {
@@ -39,7 +40,9 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     ckeckoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
-                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+                <Route 
+                    path={this.props.match.path + '/contact-data'} 
+                    component={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         )
     }
