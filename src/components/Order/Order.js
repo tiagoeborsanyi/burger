3c36762b3be724d082bb13ac177938fd565d41a0
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-05-13 19:40:45 -0300

    create componets to orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = props => (
+    <div className={classes.Order}>
+        <p>Ingredients: </p>
+        <p>Price: <strong>$ 13.65</strong></p>
+    </div>
+)
+
+export default order;
