0f97a5c13c905a216d25aaec2f0a4d8f444425e8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-05-24 19:05:54 -0300

    accessing protected resources

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -34,7 +34,7 @@ export const checkAuthTimeOut = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime * 100);
+        }, expirationTime * 1000);
     };
 };
 
@@ -54,7 +54,7 @@ export const auth = (email, password, isSignup) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeOut(response.data.expireIn));
+                dispatch(checkAuthTimeOut(response.data.expiresIn));
             })
             .catch(err => {
                 dispatch(authFail(err.response.data.error));
