a8cbf255fde30a335c6edb02edd7ba3c31de1d47
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-05-27 17:14:18 -0300

    create test in reducer auth

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+configure({adapter: new Adapter()});
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+
+    it('should store the login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            token: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
