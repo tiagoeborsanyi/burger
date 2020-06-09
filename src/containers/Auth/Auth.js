0ee6ac838f2bab54475728ad4967d909f6dbe1a3
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-06-09 14:09:16 -0300

    setting the right useEffect dependecies

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -42,11 +42,12 @@ const auth = props => {
     });
     const [isSignup, setIsSignup] = useState(true);
     
+    const { buildingBurger, authRedirectPath, onSetAuthRedirectPath } = props;
     useEffect(() => {
-        if (!props.buildingBurger && props.authRedirectPath !== '/') {
-            props.onSetAuthRedirectPath();
+        if (!buildingBurger && authRedirectPath !== '/') {
+            onSetAuthRedirectPath();
         }
-    });    
+    }, [buildingBurger, authRedirectPath, onSetAuthRedirectPath]);    
 
     const inputChangeHandler = (event, controlName) => {
         const updatedControls = {
